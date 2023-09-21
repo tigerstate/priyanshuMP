66feb1be317970bb87e3b39dc21dab7c6e3aec3c
tigerstate <118536733+tigerstate@users.noreply.github.com>
2023-09-22 04:20:57 +0530

    Add files via upload
    Hellow guys

---
+++ script.js
@@ -0,0 +1,17 @@
+ScrollReveal({ reset: true,
+distance:'600px',
+duration:2000,
+delay:200
+});
+ScrollReveal().reveal('.quate,.myself,.social-icon,.class-project,.my-project,.school-image,.two-box,.two-box-1', { delay:  500,origin:'left' });
+ScrollReveal().reveal('.myjob,.two-box-2', { delay:  500,origin:'bottom' });
+ScrollReveal().reveal('.website-box', { delay:  500,origin:'bottom' });
+// ScrollReveal().reveal('.ul-list', { delay:  500,origin:'top' });
+// ScrollReveal().reveal('.myimage-box img', { delay:  500,origin:'left' });
+ScrollReveal().reveal('.about-section,.about-edu,.big-team', { delay:  500,origin:'bottom' });
+// ScrollReveal().reveal('.Myresume', { delay:  500,origin:'right' });
+
+
+
+
+
